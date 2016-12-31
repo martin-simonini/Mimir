8e2f2b51aff255af29a535ec96454394201bb8c5
Enzo Simonini <enzosimonini@Atlas.local>
2016-12-30 21:31:09 -0600

    Added to routes and made Dashboard.html

--- collections/projects.js
+++ collections/projects.js
@@ -1,3 +1,15 @@
+/*
+  Notes:
+    This projects collection represents each tour the user will create. This will
+    be what the user will create for tours and information centers.
+
+    Needed Info:
+      Pins: All the locations that the user would like to highlight on their
+            tours
+     Route: (optional) the series of pins that the user will follow.
+     Name: Name of the project.
+*/
+
 Projects = new Mongo.Collection('project');
 
 ProjectSchema = new SimpleSchema({
