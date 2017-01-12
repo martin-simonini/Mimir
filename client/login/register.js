edb703154d58dcf809fb71c5c8ddd8f9d1307296
Martin Simonini <ssimonm356@c332-ml-mc1-271.omahaps.ops.org>
2017-01-12 13:18:32 -0600

    added user register and worked on the navbar

---
+++ client/login/register.js
@@ -0,0 +1,13 @@
+Template.register.events({
+  'submit form': function(event){
+    event.preventDefault();
+    var username = $('[name=username]').val();
+    var email = $('[name=email]').val();
+    var password = $('[name=password]').val();
+    Accounts.createUser({
+      username: username,
+      email: email,
+      password: password
+    });
+  }
+});
