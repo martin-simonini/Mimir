8e6c74c321b5b16990a9df46431efa5a7ac859f8
Noah L Giron <sgironn771@c332-ml-mc1-257.omahaps.ops.org>
2017-02-07 13:08:55 -0600

    Worked on aligning pictures and h1

--- client/projects/projectEdit/script/project.js
+++ client/projects/projectEdit/script/project.js
@@ -8,7 +8,3 @@ Template.project.helpers({
         return Projects.findOne({_id: id});
     }
 });
-<<<<<<< HEAD
-// Session.set("editing", "false");
-=======
->>>>>>> fdd13e3ab599ebc8d26afe91bb8afe5bad565dcd
