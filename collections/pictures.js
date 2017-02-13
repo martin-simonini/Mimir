0376fc3e17022d919952e9e3c8db6fe33d24a1d1
Martin Simonini <ssimonm356@c332-ml-mc1-271.omahaps.ops.org>
2017-02-13 13:15:52 -0600

    Did a little work on the image collection

--- collections/pictures.js
+++ collections/pictures.js
@@ -3,4 +3,35 @@ EntriesImages = new FS.Collection('images', {
   stores: [new FS.Store.GridFS('EntriesImages')]
 });
 
-https://forums.meteor.com/t/upload-images-with-gridfs/16617
+ImagesSchema = new SimpleSchema({
+  image: {
+    type: String,
+    label: "Image"
+  },
+  author: {
+    type: String,
+    label: "Author",
+    autoValue: function(){
+      return this.userId;
+    },
+    autoform: {
+      type: "hidden"
+    }
+  },
+  pin: {
+    type: String,
+    autoform: {
+      type: 'hidden',
+    }
+  }
+});
+
+Images.attachSchema(ImagesSchema);
+
+// Publishing the collections (server folder)
+Meteor.publish('images', function(){
+    return Images.find({author: this.userId});
+});
+Meteor.publish('EntriesImages', function(){
+    return EntriesImages.find();
+});
