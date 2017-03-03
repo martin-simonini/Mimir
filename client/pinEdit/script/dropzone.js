1527aa03c06b83b0515c7bb433bd024b00513af8
Martin Simonini <soy_martin98@yahoo.com>
2017-03-03 14:53:05 -0600

    Worked on pin edit page

--- client/pinEdit/script/dropzone.js
+++ client/pinEdit/script/dropzone.js
@@ -6,11 +6,34 @@ Template.dropzone.events({
       var newFile = new FS.File(file);
       newFile.username = user.username;
       newFile.userId = user._id;
+      var success = false;
 
       Images.insert(newFile, function(error,fileObj){
         if(error){
+          Videos.insert(newFile, function(error,fileObj){
+            if(error){
+              Pdfs.insert(newFile, function(error,fileObj){
+                if(error){
+                  toastr.error("Upload failed... please try again.");
+                  console.log(error);
+                }else{
+                  toastr.success('Upload succeeded!');
+                  success = true;
+                }
+              });
+              if(!success){
+                toastr.error("Upload failed... please try again.");
+                console.log(error);
+              }
+            } else{
+              toastr.success('Upload succeeded!');
+              success = true;
+            }
+          });
+          if(!success){
           toastr.error("Upload failed... please try again.");
           console.log(error);
+          }
         } else {
           toastr.success('Upload succeeded!');
         }
--- client/pinEdit/script/pinEdit.js
+++ client/pinEdit/script/pinEdit.js
@@ -1,10 +1,10 @@
 var images = Images.find().fetch();
+var videos = Videos.find().fetch();
+var pdf = Pdfs.find().fetch();
+
 Template.pinEdit.helpers({
   pin: function(){
     return Pins.findOne({_id: Session.get("pin-id")});
-  },
-  images: function(){
-    return images;
   },
   name: function(){
     return images[original].name;
@@ -14,5 +14,34 @@ Template.pinEdit.helpers({
   },
   'images': function() {
     return Images.find();
+  },
+  numOfVideos: function(){
+    return Videos.find().count();
+  },
+  'videos': function(){
+    return videos.find();
+  },
+  numOfPdf: function(){
+    return Pdfs.find().count();
+  },
+  pdf: function(){
+    return pdf;
+  }
+});
+
+Template.pinEdit.events({
+  'click .submit': function(e){
+    e.preventDefault();
+    var name = $('[name=name]').val();
+    var Latitude = $('[Latitude=Latitude]').val();
+    var Longitude = $('[Longitude=Longitude]').val();
+    var desc = $('[desc=desc]').val();
+    Pins.update(Session.get("pin-id"),{
+      $set:{
+      name: name,
+      Latitude: Latitude,
+      Longitude: Longitude,
+      desc: desc,
+    }});
   }
 })
